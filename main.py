48f330eb34d2230b8f4ff782612c49b61cb7159e
Justin Bornais <justin.bornais@gmail.com>
2022-11-02 15:27:12 -0400

    Made an update.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from moviepy.editor import *
+from PIL import Image
+from PIL import ImageDraw
 import os
 
 if __name__ == '__main__':
     
     clip = VideoFileClip('videos/sample.mp4').rotate(180)
-    clip.write_videofile('sup.mp4', threads=16, codec="h264_nvenc")
+    clip.write_videofile('sup.mp4', threads=16, codec="h264", verbose=False, logger=None)
     print("Epic.")
