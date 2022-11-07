e31b29cba1c770c60b9c368e9030f0b5e5d3cd92
Justin Bornais <justin.bornais@gmail.com>
2022-11-07 18:09:38 -0500

    Added image editing capabilities.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Online Video Maker
 
+This is a simple web application that allows people to create simple slideshow videos. You can add images, text per image and audio over the slideshow.
+
 In order for this to work, be sure to install <b>PyGame</b> and <b>MoviePy</b>.
--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from moviepy.editor import *
 from PIL import Image
-from PIL import ImageDraw
+from PIL import ImageDraw, ImageFont
 import os
+
+FONT = 'open-sans/OpenSans-Regular.ttf'
 
 if __name__ == '__main__':
     
-    clip = VideoFileClip('videos/sample.mp4').rotate(180)
-    clip.write_videofile('sup.mp4', threads=16, codec="h264", verbose=False, logger=None)
+    # Open an Image
+    img = Image.open('images/Donald_Trump_official_portrait.jpg')
+    
+    # Call draw Method to add 2D graphics in an image
+    I1 = ImageDraw.Draw(img)
+
+    # Custom font style and font size
+    myFont = ImageFont.truetype(FONT, 400)
+    
+    # Add Text to an image
+    print(f'{img.width} {img.height}')
+    I1.text((int(img.width * 0.005), int(img.height * 0.7)), "HBD NICK!", fill=(255, 255, 255), font=myFont)
+    
+    # Display edited image
+    img.show()
+    
+    # Save the edited image
+    img.save("car2.png")
+    # clip = VideoFileClip('videos/sample.mp4').rotate(180)
+    # clip.write_videofile('sup.mp4', threads=16, codec="h264", verbose=False, logger=None)
     print("Epic.")
